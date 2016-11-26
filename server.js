9d087e80fa399e5e1b069439511512326743ed1c
apisit <basetdd2416@gmail.com>
2016-11-27 02:55:03 +0700

    update config

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(function (req,res,next) {
+  if(req.headers['x-forwarded-proto' === 'http']) {
+    next();
+  } else {
+    res.redirect('https://' + req.hostname + req.url);
+  }
+});
+
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
