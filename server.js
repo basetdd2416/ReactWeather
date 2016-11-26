bdd52781d6fc419b656813c5b91aac7ed38501e2
apisit <basetdd2416@gmail.com>
2016-11-27 02:52:31 +0700

    update port config

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log('Express server is up on port '+ port);
+  console.log('Express server is up on port '+ PORT);
 });
