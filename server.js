337fabe60c4dba991bd0cee2284dc2c0fec4e3d1
apisit <basetdd2416@gmail.com>
2016-11-27 03:15:49 +0700

    fix bug

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express');
+var express = require('express');
 
 // Create our app
-const app = express();
+var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req,res,next) {
-  if(req.headers['x-forwarded-proto'] === 'https') {
-      res.redirect('https://' + req.hostname + req.url);
-  } else {
-    next();
-  }
+app.use(function (req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
 });
 
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log('Express server is up on port '+ PORT);
+    console.log('Express server is up on port ' + PORT);
 });
